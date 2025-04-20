b64fd7f2a873c19864777b016ffbcd61d9c0f3f2
TaguchiRei <ren.len2426@gmail.com>
2025-04-20 16:12:45 +0900

    [Add]ホスト接続作成中

--- Assets/Script/MultiPlay/StartMultiPlayManager.cs
+++ Assets/Script/MultiPlay/StartMultiPlayManager.cs
@@ -2,17 +2,22 @@ using Cysharp.Threading.Tasks;
 using GamesKeystoneFramework.MultiPlaySystem;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Serialization;
 
 public class StartMultiPlayManager : MultiPlayManagerBase
 {
     private static readonly int ConnectionSelect = Animator.StringToHash("ConnectionSelect");
     [SerializeField] private GameObject _loadingObject;
-    private UniTask<bool> _initialize;
-    private int _connectionPhase;
-    [SerializeField] private Animator _animator;
 
     [SerializeField] private GameObject _errorMessageField;
     private TextMeshProUGUI _errorMessage;
+    
+    [SerializeField] private Animator _StartCanvasAnimator;
+    
+    private UniTask<bool> _initialize;
+    private UniTask<bool> _connectionHost; 
+    private int _connectionPhase;
     
     public void Awake()
     {
@@ -20,22 +25,23 @@ public class StartMultiPlayManager : MultiPlayManagerBase
         _loadingObject.SetActive(true);
         _connectionPhase = 0;
         _initialize = ServicesInitialize();
+        Debug.Log("MultiPlayManagerBase.Awake");
     }
 
     void Update()
     {
         switch (_connectionPhase)
         {
-            case 0:
-                var awaiter = _initialize.GetAwaiter();
-                if (awaiter.IsCompleted)
+            case 1:
+                var initializeAwaiter = _initialize.GetAwaiter();
+                if (initializeAwaiter.IsCompleted)
                 {
-                    var result = awaiter.GetResult();
+                    var result = initializeAwaiter.GetResult();
                     if (result)
                     {
-                        _connectionPhase = 1;
+                        _connectionPhase = 0;
                         _loadingObject.SetActive(false);
-                        _animator.SetTrigger(ConnectionSelect);
+                        _StartCanvasAnimator.SetTrigger(ConnectionSelect);
                     }
                     else
                     {
@@ -43,10 +49,31 @@ public class StartMultiPlayManager : MultiPlayManagerBase
                         _errorMessage.text = "オンラインサービスの初期化に失敗しました　\n ネット環境を確認してから再起動してください";
                     }
                 }
-
                 break;
-            case 1:
+            case 2:
+                var connectionHostAwaiter = _connectionHost.GetAwaiter();
+                if (connectionHostAwaiter.IsCompleted)
+                {
+                    var result = connectionHostAwaiter.GetResult();
+                    if (result)
+                    {
+                        SceneManager.LoadScene("HostScene");
+                    }
+                }
+                break;
+            default:
                 break;
         }
+        if (_connectionPhase == 1)
+        {
+
+        }
+    }
+
+    public void ConnectionHost()
+    {
+        
+         _connectionHost = HostConnect();
+         _connectionPhase = 2;
     }
 }
