62612c9ba6875cd87839f6b4b3da19b8b90dc09e
TaguchiRei <ren.len2426@gmail.com>
2025-04-21 17:12:53 +0900

    [Add] ロビーにプレイヤーが参加するのを待機する

--- Assets/Script/MultiPlay/HostPlayManager.cs
+++ Assets/Script/MultiPlay/HostPlayManager.cs
@@ -1,4 +1,7 @@
+using Cysharp.Threading.Tasks;
 using GamesKeystoneFramework.MultiPlaySystem;
+using Unity.Netcode;
+using Unity.Services.Lobbies;
 using Unity.Services.Lobbies.Models;
 using UnityEngine;
 
@@ -14,6 +17,35 @@ public class HostPlayManager : MultiPlayManagerBase
         _hostPlayerInstance = Instantiate(_hostPlayerPrefab, Vector3.zero, Quaternion.identity);
         _joinedLobby = LobbyRetention.Instance.JoinedLobby;
         Debug.Log($"LobbyName : {_joinedLobby.Name} LobbyID : {_joinedLobby.Id}");
+        NetworkManager.Singleton.OnClientConnectedCallback += _ => Lock();
     }
-    
+
+    void Update()
+    {
+        
+    }
+
+    private void Lock()
+    {
+        _ = LobbyLock();
+    }
+
+    /// <summary>
+    /// ロビーをロックする
+    /// </summary>
+    private async UniTask LobbyLock()
+    {
+        var updateOptions = new UpdateLobbyOptions
+        {
+            IsLocked = true
+        };
+        
+        await LobbyService.Instance.UpdateLobbyAsync(_joinedLobby.Id, updateOptions);
+    }
+
+    private enum LobbyState
+    {
+        WaitingForClient,
+        Started,
+    } 
 }
