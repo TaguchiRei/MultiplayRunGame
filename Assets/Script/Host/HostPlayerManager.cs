e7298cc475dd73bfd43fd16d906edc5da91fdd0f
TaguchiRei <ren.len2426@gmail.com>
2025-04-23 11:56:21 +0900

    [Add]各種入力についての動きを実装

--- Assets/Script/Host/HostPlayerManager.cs
+++ Assets/Script/Host/HostPlayerManager.cs
@@ -1,3 +1,7 @@
+using System;
+using GamesKeystoneFramework.Attributes;
+using Unity.Netcode;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class HostPlayerManager : MonoBehaviour
@@ -5,8 +9,69 @@ public class HostPlayerManager : MonoBehaviour
     [SerializeField] private Rigidbody _rigidbody;
     [SerializeField] private HostPlayerAnimationManager _animationManager;
     [SerializeField] private InputManager _inputManager;
+    
+    [SerializeField, Grouping] private PlayerData _playerData;
+    
+    private Vector3 _moveDirection = Vector3.zero;
+
+    private bool _jump;
+    private bool _onGround;
+    
+    /// <summary>
+    /// 直近のジャンプのタイミング。
+    /// </summary>
+    [ReadOnlyInInspector]public NetworkVariable<float> _latestJumpTime;
+
     void GameStart()
     {
-        
+        _moveDirection = new Vector3(0, 0, 10);
+        _inputManager.OnMove += Move;
+        _inputManager.OnJump += Jump;
+        _inputManager.OnJumpEnd += JumpEnd;
+    }
+    
+    private void FixedUpdate()
+    {
+        if (NetworkManager.Singleton.IsHost)//ホストの場合自分で動かす
+        {
+            _rigidbody.linearVelocity = new Vector3(_moveDirection.x, _rigidbody.linearVelocity.y, _rigidbody.linearVelocity.z);
+        }
+    }
+
+    private void Move(Vector2 inputDirection)
+    {
+        _moveDirection.x = inputDirection.x;
+    }
+
+    private void Jump()
+    {
+        _onGround = false;
+        _rigidbody.AddForce(0,_playerData.jumpForce,0, ForceMode.Impulse);
+    }
+
+    private void JumpEnd()
+    {
+        if (_rigidbody.linearVelocity.y > 0)
+        {
+            _rigidbody.linearVelocity = new Vector3(
+                _rigidbody.linearVelocity.x,
+                _rigidbody.linearVelocity.y / 2,
+                _rigidbody.linearVelocity.z);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground") && !_onGround)
+        {
+            _onGround = true;
+        }
+    }
+
+    [Serializable]
+    private struct PlayerData
+    {
+        public float moveSpeed;
+        public float jumpForce;
     }
 }
--- Assets/Script/Host/InputManager.cs
+++ Assets/Script/Host/InputManager.cs
@@ -6,8 +6,6 @@ public class InputManager : MonoBehaviour
     public Action<Vector2> OnMove;
     public Action OnJump;
     public Action OnJumpEnd;
-    public Action OnDash;
-    public Action OnDashEnd;
     
     MultiPlayerInputActions _inputActions;
 
@@ -18,8 +16,6 @@ public class InputManager : MonoBehaviour
         _inputActions.Player.Move.performed += ctx => OnMove?.Invoke(ctx.ReadValue<Vector2>());
         _inputActions.Player.Jump.started += ctx => OnJump?.Invoke();
         _inputActions.Player.Jump.canceled += ctx => OnJumpEnd?.Invoke();
-        _inputActions.Player.Sprint.started += ctx => OnDash?.Invoke();
-        _inputActions.Player.Sprint.canceled += ctx => OnDashEnd?.Invoke();
         
         _inputActions.Enable();
     }
