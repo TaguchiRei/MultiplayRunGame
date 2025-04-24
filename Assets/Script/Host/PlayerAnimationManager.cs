ef518ebe5e280fc4269e8c805bb3c694cd6a9563
TaguchiRei <ren.len2426@gmail.com>
2025-04-24 11:42:31 +0900

    [Add]入力が直接かかわるものはInputManagerのコールバックに追加

--- Assets/Script/Host/PlayerAnimationManager.cs
+++ Assets/Script/Host/PlayerAnimationManager.cs
@@ -7,9 +7,12 @@ public class PlayerAnimationManager : MonoBehaviour
     private static readonly int Move = Animator.StringToHash("Move");
     private static readonly int Run = Animator.StringToHash("Run");
     private static readonly int Jump = Animator.StringToHash("Jump");
+    private static readonly int FB = Animator.StringToHash("FB");
+    private static readonly int LR = Animator.StringToHash("LR");
 
     [SerializeField] GameObject _player;
-
+    
+    [SerializeField] InputManager _inputManager;
     private Animator _animator;
     
     [ReadOnlyInInspector] public bool SlowMotion;
@@ -19,19 +22,22 @@ public class PlayerAnimationManager : MonoBehaviour
         _animator = _player.GetComponent<Animator>();
     }
 
-    private void AnimationStart()
+    public void LRFBUpdate(Vector2 moveVector)
+    {
+        _animator.SetFloat(FB, 1);
+        _animator.SetFloat(LR, moveVector.x);
+    }
+
+    public void AnimationStart()
     {
         _animator.SetBool(Move, true);
-    }
-    
-
-    public void RunStart()
-    {
         _animator.SetBool(Run, true);
+        _inputManager.OnJump += StartJump;
+        _inputManager.OnMove += LRFBUpdate;
     }
-    
 
-    public void StartJump()
+
+    private void StartJump()
     {
         _animator.SetBool(Jump, true);
     }
