08f426088edcf4586a59342a1aadbc832c4dbf4f
TaguchiRei <ren.len2426@gmail.com>
2025-06-04 18:07:52 +0900

    [update]障害物を生成するスクリプトを追加

--- Assets/Script/Ground/GroundManager.cs
+++ Assets/Script/Ground/GroundManager.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using Cysharp.Threading.Tasks;
+using TMPro;
 using Unity.Netcode;
 using UnityEngine;
 
@@ -9,10 +10,14 @@ public class GroundManager : MonoBehaviour
     [SerializeField, Range(1, 10)] private int _groundCount;
     [SerializeField] private float _speed;
     [SerializeField] private GameObject _groundObject;
+    [SerializeField] private int _obstacleSpawnTiming;
+    [SerializeField] private GameObject[] _obstacleObjects;
+    [SerializeField] private Vector3 _obstaclePool;
 
     private List<Transform> _groundObjects;
+    private Queue<GameObject>[] _obstacleInstances;
+    private int _obstacleSpawnCounter;
     private bool _isStarted = false;
-
     private const float GroundSize = 60; //グラウンドの大きさは60
 
     public void GameStart()
@@ -26,13 +31,23 @@ public class GroundManager : MonoBehaviour
 
     private async UniTask Initialize()
     {
-        var result = await InstantiateAsync(_groundObject, _groundCount, Vector3.zero, Quaternion.identity);
-        _groundObjects = result.Select(o => o.transform).ToList();
+        var groundResult = await InstantiateAsync(_groundObject, _groundCount, Vector3.zero, Quaternion.identity);
+        _groundObjects = groundResult.Select(o => o.transform).ToList();
         _isStarted = true;
         for (int i = 0; i < _groundObjects.Count; i++)
         {
             _groundObjects[i].position = Vector3.forward * (i * GroundSize);
             _groundObjects[i].GetComponent<NetworkObject>().Spawn();
+        }
+
+        for (int i = 0; i < _obstacleObjects.Length; i++)
+        {
+            var obstacleResult = await InstantiateAsync(_obstacleObjects[i], 2, _obstaclePool, Quaternion.identity);
+
+            foreach (var obstacle in obstacleResult)
+            {
+                _obstacleInstances[i].Enqueue(obstacle);
+            }
         }
     }
 
@@ -50,6 +65,15 @@ public class GroundManager : MonoBehaviour
         {
             _groundObjects.Add(_groundObjects[0]);
             _groundObjects.RemoveAt(0);
+            if (_obstacleSpawnCounter >= _obstacleObjects.Length)
+            {
+                _obstacleSpawnCounter = 0;
+                
+            }
+            else
+            {
+                _obstacleSpawnCounter++;
+            }
         }
     }
 }
