c2b82ca7993ed6c45bb12b5159c5f1b4528c3bb4
TaguchiRei <ren.len2426@gmail.com>
2025-06-07 19:46:26 +0900

    [update]バリア用スクリプト完成

--- Assets/Script/Ground/BarrierObject.cs
+++ Assets/Script/Ground/BarrierObject.cs
@@ -1,16 +1,24 @@
 using System;
 using GamesKeystoneFramework.Attributes;
+using GamesKeystoneFramework.MultiPlaySystem;
+using Unity.Netcode;
 using UnityEngine;
 
 public class BarrierObject : MonoBehaviour
 {
     [SerializeField,Grouping] private bool _isHostBarrier;
+    
+    MultiPlayRadioTower _multiPlayRadioTower;
+
+    private void OnEnable()
+    {
+        _multiPlayRadioTower = FindAnyObjectByType<MultiPlayRadioTower>();
+    }
 
     private void OnCollisionEnter(Collision other)
     {
-        if (other.gameObject.CompareTag("HostBarrier") == _isHostBarrier)
-        {
-            
-        }
+        if(!NetworkManager.Singleton.IsHost) return;//バリアへの衝突判定はホスト側のみで行う
+
+        _multiPlayRadioTower.Send(other.gameObject.CompareTag("HostBarrier") == _isHostBarrier ? 3 : 4);
     }
 }
